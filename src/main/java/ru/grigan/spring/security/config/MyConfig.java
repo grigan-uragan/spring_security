0424ed5b697b46fa69bbace5b8b74f6725199412
grigan-Uragan <prischepny@mail.ru>
2021-03-27 13:16:48 +0300

    save simple password in db

--- src/main/java/ru/grigan/spring/security/config/MyConfig.java
+++ src/main/java/ru/grigan/spring/security/config/MyConfig.java
@@ -1,11 +1,15 @@
 package ru.grigan.spring.security.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 @Configuration
 @ComponentScan(basePackages = {"ru.grigan.spring.security"})
@@ -19,5 +23,19 @@ public class MyConfig {
         viewResolver.setPrefix("/WEB-INF/view/");
         viewResolver.setSuffix(".jsp");
         return viewResolver;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("org.postgresql.Driver");
+            dataSource.setJdbcUrl("jdbc:postgresql://127.0.0.1:5432/spring_db");
+            dataSource.setUser("postgres");
+            dataSource.setPassword("password");
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+        return dataSource;
     }
 }
--- src/main/java/ru/grigan/spring/security/config/MySecurityConfig.java
+++ src/main/java/ru/grigan/spring/security/config/MySecurityConfig.java
@@ -1,5 +1,6 @@
 package ru.grigan.spring.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,15 +8,21 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class MySecurityConfig  extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("grigan").password("grigan").roles("EMPLOYEE"))
-                .withUser(userBuilder.username("john").password("doe").roles("MANAGER", "HR"))
-                .withUser(userBuilder.username("gurgen").password("purgen").roles("HR"));
+//        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//        auth.inMemoryAuthentication()
+//                .withUser(userBuilder.username("grigan").password("grigan").roles("EMPLOYEE"))
+//                .withUser(userBuilder.username("john").password("doe").roles("MANAGER", "HR"))
+//                .withUser(userBuilder.username("gurgen").password("purgen").roles("HR"));
+        auth.jdbcAuthentication().dataSource(dataSource);
     }
 
     @Override
